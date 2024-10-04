5948cc0824ec4197600224907922be4d00ef37da
bepa <pavel.belov@zuhlke.com>
2024-10-04 15:45:25 +0300

    lesson 10: exceptions

--- src/main/java/com/example/workflow/delegates/PrepareAlliesArmyServiceTaskDelegate.java
+++ src/main/java/com/example/workflow/delegates/PrepareAlliesArmyServiceTaskDelegate.java
@@ -1,19 +1,24 @@
 package com.example.workflow.delegates;
 
+import org.camunda.bpm.engine.delegate.BpmnError;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
-
-import java.util.Random;
 
 @Component("prepareAlliesArmyServiceTask")
 public class PrepareAlliesArmyServiceTaskDelegate implements JavaDelegate {
 	@Override
 	public void execute(DelegateExecution delegateExecution) throws Exception {
 
-		var random = new Random();
+		var inputVariable = delegateExecution.getVariable("allies");
+		if (inputVariable == null) {
+			throw new BpmnError("count_of_allies_error");
+		}
 
-		var allies = random.nextInt(100) + 1;
+		var allies = Integer.parseInt(inputVariable.toString());
+		if ( allies < 10 || allies > 100 ) {
+			throw new BpmnError("count_of_allies_error");
+		}
 
 		// set variables to Camunda context
 		delegateExecution.setVariable("allies", allies);
