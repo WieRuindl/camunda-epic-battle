2448b08391f0be51c139ca76881daa176d1fc575
bepa <pavel.belov@zuhlke.com>
2024-10-04 11:50:08 +0300

    lesson 8: process input variables

--- src/main/java/com/example/workflow/delegates/PrepareBattlefieldServiceTaskDelegate.java
+++ src/main/java/com/example/workflow/delegates/PrepareBattlefieldServiceTaskDelegate.java
@@ -2,6 +2,7 @@ package com.example.workflow.delegates;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,9 +10,16 @@ import java.util.Collections;
 
 @Component("prepareBattlefieldServiceTask")
 public class PrepareBattlefieldServiceTaskDelegate implements JavaDelegate {
+
+	@Value("${default_battle_cells_count}")
+	private int defaultBattleCellsCount;
+
 	@Override
 	public void execute(DelegateExecution delegateExecution) throws Exception {
-		var battleCellsCount = 10;
+		// get input process variable from Camunda context
+		var inputVariable = delegateExecution.getVariable("battle_cells_count");
+		var battleCellsCount = (inputVariable == null) ? defaultBattleCellsCount : (int) inputVariable;
+		System.out.println("battleCellsCount: " + battleCellsCount);
 		var battlefield = new ArrayList<>(Collections.nCopies(battleCellsCount, true));
 
 		delegateExecution.setVariable("battlefield", battlefield);
